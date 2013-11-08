de30770871adcb4eb88a6fabe82dfff790f709cb
Stefuniverse <Stefan.Pawlowski@t-online.de>
2013-11-08 18:24:03 +0100

    Completed restruction for divede Layout and function and better multicore support

--- src/medien.java
+++ src/medien.java
@@ -1,56 +1,25 @@
-//By Stefan Pawlowski updated; 26.10.2013 Uniplayer
-
-//Imports File
-
 
 import javafx.scene.media.MediaPlayer;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.media.Media;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.scene.media.MediaView;
 
+import java.io.File;
+import java.net.MalformedURLException;
 
-
-import java.util.*;
+import javax.swing.JFileChooser;
 
 //Buttons and layout
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.event.*;
-import javafx.scene.input.*;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.paint.Color;
 
 public class medien extends Application {
-	
-	static final String Layout_Main1 = ("-fx-background-color: #333333;"
-				+ "-fx-text-fill: white;"
-				+ "-fx-font-size: 12;");
-	
-	static final String Layout_Main2 = ("-fx-background-color: #666666;"
-			+ "-fx-text-fill: white;"
-			+ "-fx-font-size: 12;");
-	
-	
-	static final String Layout_active1 = ("-fx-text-fill: #7DA1EB;"
-			+ "-fx-background-color: #333333;"
-			+ "-fx-font-size: 12;");
-	
-	static final String Layout_active2 = ("-fx-text-fill: #7DA1EB;"
-			+ "-fx-background-color: #666666;"
-			+ "-fx-font-size: 12;");
-	
-	static final String Layout_Action1 = ("-fx-text-fill: #FF6600;"
-			+ "-fx-background-color: #333333;"
-			+ "-fx-font-size: 12;");
-	
-	static final String Layout_Action2 = ("-fx-text-fill: #FF6600;"
-			+ "-fx-background-color: #666666;"
-			+ "-fx-font-size: 12;");
 	
 	static final String Layout_Playlist = ("-fx-background-color: #333333;"
 										+ "-fx-border-width: 2;"
@@ -62,43 +31,41 @@ public class medien extends Application {
 	
 	static final double Playlist_Width = 300.0;
 	
-	MediaView mediaview = new MediaView();
+	
 	final BorderPane root = new BorderPane();
     final HBox bb = new HBox();
     final HBox pb = new HBox();
-    final VBox list = new VBox();
     final VBox Menu = new VBox();
+    final VBox list = new VBox();
 
     final Button startpause = new Button();
     final Button playatonce = new Button();
     final Button prev = new Button();
     final Button next = new Button();
     final Button playlater = new Button();
-    Slider Songp = new Slider();
     
 	int[] width = null;
 	int[] height = null;
     MediaPlayer MP;
+    final Mediafunctions bind = new Mediafunctions();
+    int current = 0;
 
 	@Override
 	public void start(final Stage pS)
     {
-		final Mediabinder bind = new Mediabinder(pS);
         startpause.setText("Pause");
         playatonce.setText("Play at once");
         prev.setText("Prev");
         next.setText("Next");
         playlater.setText("PLay later");
         pS.setTitle("Uniplayer-Alpha");
-        Songp = Mip.getslider();
-        Mip.start();
         
 					
         startpause.setOnAction(new EventHandler<ActionEvent>() {
         	@Override 
         	public void handle(ActionEvent event) {
         		
-                playpause();
+                bind.playpause(startpause);
                 
             }
         });
@@ -108,8 +75,8 @@ public class medien extends Application {
             @Override
             public void handle(ActionEvent event) {
             	
-            	bind.start();
-            	updateview(pS, Title.size()-1);
+            	choosefile(pS);
+            	bind.updateview(pS, bind.getnames().size()-1);
             	
             }
         });
@@ -119,7 +86,7 @@ public class medien extends Application {
             @Override
             public void handle(ActionEvent event) {
             	
-            	if ((0 != bind.getcurrent()) && (!Title.isEmpty())) {
+            	if ((0 != bind.getcurrent()) && (!bind.getnames().isEmpty())) {
             		
             		bind.updateview (pS,(bind.getcurrent())-1);
             	}
@@ -131,7 +98,7 @@ public class medien extends Application {
             @Override
             public void handle(ActionEvent event) {
             	
-            	if (((Title.size()-1) != bind.getcurrent()) && (!Title.isEmpty())) {
+            	if (((bind.getnames().size()-1) != bind.getcurrent()) && (!bind.getnames().isEmpty())) {
             		
             		bind.updateview(pS, (bind.getcurrent()+1));
             		
@@ -145,14 +112,14 @@ public class medien extends Application {
             @Override
             public void handle(ActionEvent event) {
             	
-            	bind.start();
+            	choosefile(pS);
     	
             }
         });
         
         //Layout
         bb.getChildren().addAll(startpause, playatonce, playlater, prev, next);
-        pb.getChildren().addAll(Songp);
+        pb.getChildren().addAll(bind.getprog());
         Menu.getChildren().addAll(pb, bb);
         bb.setPadding(new Insets(15, 12, 15, 12));
         pb.setPadding(new Insets(15, 30, 15, 30));
@@ -162,7 +129,7 @@ public class medien extends Application {
         list.setPadding(new Insets(20, 0, 0, 5));
         root.setBottom(Menu);
         root.setRight(list);
-        root.setCenter(mediaview);
+        root.setCenter(bind.getmediaview());
         Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
 
@@ -171,7 +138,7 @@ public class medien extends Application {
         pS.setWidth(bounds.getWidth());
         pS.setHeight(bounds.getHeight());
        
-        Songp.setMinWidth(pS.getWidth()-80);
+        bind.getprog().setMinWidth(pS.getWidth()-80);
         list.setMinWidth(Playlist_Width);
         list.setMinHeight(pS.getHeight()*0.8);
         list.setStyle(Layout_Playlist);
@@ -180,8 +147,8 @@ public class medien extends Application {
         Menu.setMaxHeight(pS.getHeight()*0.2);
         Menu.setStyle(Layout_Panel);
         Menu.setAlignment(Pos.BOTTOM_CENTER);
-        mediaview.setFitWidth(pS.getWidth() - Playlist_Width);
-		mediaview.setFitHeight(pS.getHeight()*0.8);
+        bind.getmediaview().setFitWidth(pS.getWidth() - Playlist_Width);
+        bind.getmediaview().setFitHeight(pS.getHeight()*0.8);
 
         Scene scene = new Scene(root);
         scene.setFill(Color.BLACK);
@@ -190,26 +157,42 @@ public class medien extends Application {
 	        	
 	}
 	
-	void playpause() {
+	@SuppressWarnings("deprecation")
+	public void choosefile(Stage pS) {
+		File read;
+		JFileChooser fileChooser = new JFileChooser();
 		
-		if ("Pause".equals(startpause.getText())) {
-        	if (MP != null) {
-        		MP.pause();
-        		startpause.setText("Play");
-        	}
-        }
-        else {
-        	if (MP != null)
-        	{
-        		MP.play();
-        		startpause.setText("Pause");
-        	}
-        }
-	}
+					fileChooser.setFileSelectionMode(
+		           JFileChooser.FILES_ONLY );
+		        int result = fileChooser.showOpenDialog( fileChooser);
+		        
+
+		        // user clicked Cancel button on dialog
+		        if ( result == JFileChooser.CANCEL_OPTION )
+		        {
+		        	
+		        }
+		        else
+		        {
+		           read = fileChooser.getSelectedFile();
+		           try
+		           {   
+		        		   
+		        	bind.getURL().add(read.toURL().toExternalForm().replace(" ", "%20"));
+		        	   
+
+		        	   bind.getnames().add(bind.Labelbuilder(read.getName(),pS));
+		               
+		           		list.getChildren().addAll(bind.getnames().get((bind.getnames().size()-1)));
+		           }
+		           catch (MalformedURLException ex)
+		           {
+		        	   System.out.println("URL konnte nicht geholt werden");
+
+		           };
+		       }	
+			}
 	
-	MediaPlayer getplayer() {
-		return MP;
-	}
 	
 	
 	public static void main(String args[]) {
